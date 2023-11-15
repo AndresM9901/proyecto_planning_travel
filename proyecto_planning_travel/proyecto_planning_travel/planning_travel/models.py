4a14cc055c555cd122e097fe3bbc78f91bd27339
DavidCWork <dcorrea070@soy.sena.edu.co>
2023-11-15 15:36:04 -0500

    Actualizando rama david

---
+++ proyecto_planning_travel/proyecto_planning_travel/planning_travel/models.py
@@ -0,0 +1,173 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=254)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return f'{self.nombre}'
+
+class Rol(models.Model):
+    nombre = models.CharField(max_length=254)
+    descripcion = models.TextField()
+    permisos = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.nombre}'
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=254, unique=True)
+    precio = models.IntegerField()
+    inventario = models.IntegerField()
+    fecha_creacion = models.DateField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.nombre}'
+    
+class Hotel(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    direccion = models.CharField(max_length=200)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    cantidad_habitaciones = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.nombre}'
+    
+class Comodidad(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField(max_length=200)
+
+    def __str__(self):
+        return f'{self.nombre}'
+    
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=254)
+    correo = models.EmailField(max_length=254, unique=True)
+    contrasena = models.CharField(max_length=100)
+    ROLES = (
+        (1, "Administrador"),
+        (2, "Despachador"),
+        (3, "Cliente"),
+    )
+    rol = models.IntegerField(choices=ROLES, default=3)
+    foto = models.ImageField(upload_to="planning_travel/media/")
+    # baneado = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.nombre}'
+    
+class Favorito(models.Model):
+    id_hotel = models.ForeignKey(Hotel, on_delete=models.DO_NOTHING)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    fecha_agregado = models.DateField()
+
+    def __str__(self):
+        return f'{self.id_hotel}'
+    
+class Comentario(models.Model):
+    id_hotel = models.ForeignKey(Hotel, on_delete=models.DO_NOTHING)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    contenido = models.TextField()
+    fecha = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.contenido}'
+    
+class Puntuacion(models.Model):
+    id_comentario = models.ForeignKey(Comentario, on_delete=models.DO_NOTHING)
+    valoracion = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.valoracion}'
+    
+class Foto(models.Model):
+    id_hotel = models.ForeignKey(Hotel, on_delete=models.DO_NOTHING)
+    url_foto = models.ImageField(upload_to="planning_travel/media/")
+    descripcion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.id_hotel}'
+    
+class HotelComodidad(models.Model):
+    id_hotel = models.ForeignKey(Hotel, on_delete=models.DO_NOTHING)
+    id_comodidad = models.ForeignKey(Comodidad, on_delete=models.DO_NOTHING)
+    dispone = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.id_hotel}'
+    
+class HotelCategoria(models.Model):
+    id_hotel = models.ForeignKey(Hotel, on_delete=models.DO_NOTHING)
+    id_categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.id_hotel}'
+    
+class Habitacion(models.Model):
+    num_habitacion = models.IntegerField()
+    id_hotel = models.ForeignKey(Hotel, on_delete=models.DO_NOTHING)
+    ocupado = models.BooleanField()
+    capacidad_huesped = models.IntegerField()
+    tipoHabitacion = models.CharField(max_length=255)
+    foto = models.ForeignKey(Foto, on_delete=models.DO_NOTHING)
+    precio = models.DecimalField(max_digits=250, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.num_habitacion}'
+
+class Reserva(models.Model):
+    habitacion = models.ForeignKey(Habitacion, on_delete=models.DO_NOTHING)
+    fecha_llegada = models.DateField()
+    fecha_salida = models.DateField()
+    cantidadPersonas = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.habitacion}'
+
+class ReservaUsuario(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    reserva = models.ForeignKey(Reserva, on_delete=models.DO_NOTHING)
+    estado_reserva = models.CharField(max_length=255)
+    fecha_realizacion = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.fecha_realizacion}'
+
+class PerfilUsuario(models.Model):
+    id_hotel = models.ForeignKey(Hotel, on_delete=models.DO_NOTHING)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    nombre = models.CharField(max_length=255)
+    numero_contacto = models.CharField(max_length=15)
+    fotoPerfil = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.id_hotel}'
+
+class Cliente(models.Model):
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    nombre = models.CharField(max_length=255)
+    numero_contacto = models.CharField(max_length=15)
+    fotoPerfil = models.ImageField(upload_to="planning_travel/media/")
+
+    def __str__(self):
+        return f'{self.nombre}'
+
+class Reportes(models.Model):
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    nombre = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.id_usuario}'
+
+class ReporteModerador(models.Model):
+    id_reporte = models.ForeignKey(Reportes, on_delete=models.DO_NOTHING)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    fecha_inicio = models.DateTimeField()
+    fecha_fin = models.DateField()
+
+    def __str__(self):
+        return f'{self.id_reporte}'
---
+++ proyecto_planning_travel/proyecto_planning_travel/planning_travel/views.py
@@ -0,0 +1,289 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
+
+# Create your views here.
+def inicio(request):
+    return render(request, 'planning_travel/inicio.html')
+
+# Crud de Categorias
+def categorias(request):
+    # select * from categorias
+    consulta = Categoria.objects.all()
+    context = {'data': consulta}
+    return render(request, 'planning_travel/categorias/categorias.html', context)
+
+def categorias_form(request):
+    return render(request, 'planning_travel/categorias/categorias_form.html')
+
+def categorias_crear(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        descripcion = request.POST.get('descripcion')
+        try:
+            q = Categoria(
+                nombre=nombre,
+                descripcion=descripcion
+            )
+            q.save()
+            messages.success(request, "Fue agregado correctamente")
+        except Exception as e:
+            messages.error(request,f'Error: {e}')
+
+        return redirect('categorias_listar')
+    else:
+        messages.warning(request,'No se enviaron datos')
+        return redirect('categorias_listar')
+
+def categorias_eliminar(request, id):
+    try:
+        q = Categoria.objects.get(pk = id)
+        q.delete()
+        messages.success(request, 'Categoria eliminada correctamente!!')
+    except Exception as e:
+        messages.error(request,f'Error: {e}')
+
+    return redirect('categorias_listar')
+
+def categorias_formulario_editar(request, id):
+
+    q = Categoria.objects.get(pk = id)
+    contexto = {'data': q}
+
+    return render(request, 'planning_travel/categorias/categorias_form_editar.html', contexto)
+
+def categorias_actualizar(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        nombre = request.POST.get('nombre')
+        descripcion = request.POST.get('descripcion')
+        try:
+            q = Categoria.objects.get(pk = id)
+            q.nombre = nombre
+            q.descripcion = descripcion
+            q.save()
+            messages.success(request, "Fue actualizado correctamente")
+        except Exception as e:
+            messages.error(request,f'Error: {e}')
+    else:
+        messages.warning(request,'No se enviaron datos')
+        
+    return redirect('categorias_listar')
+
+
+def index(request):
+    return render(request, 'planning_travel/login/login.html')
+
+# Crud de Usuarios
+def usuarios(request):
+    q = Usuario.objects.all()
+    contexto = {'data': q}
+    return render(request, 'planning_travel/login/usuarios.html', contexto)
+
+def usuarios_form(request):
+    q = Rol.objects.all()
+    contexto = {'data': q}
+    return render(request, 'planning_travel/login/usuarios_form.html', contexto)
+
+def usuarios_crear(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        correo = request.POST.get('correo')
+        contrasena = request.POST.get('contrasena')
+        rol = Usuario.objects.get(pk=request.POST.get("rol"))
+        foto = request.FILES.get('foto')
+        try:
+            q = Usuario(
+                nombre=nombre,
+                correo=correo,
+                contrasena=contrasena,
+                rol=rol,
+                foto=foto,
+            )
+            q.save()
+            messages.success(request, "Fue actualizado correctamente")
+        except Exception as e:
+            messages.error(request,f'Error: {e}')
+
+        return redirect('usuarios_listar')
+    else:
+        messages.warning(request,'No se enviaron datos')
+        return redirect('usuarios_listar')
+    
+def usuarios_eliminar(request, id):
+    try:
+        q = Usuario.objects.get(pk = id)
+        q.delete()
+        messages.success(request, 'Usuario eliminado correctamente!!')
+    except Exception as e:
+        messages.error(request,f'Error: {e}')
+
+    return redirect('usuarios_listar')
+
+def usuarios_form_editar(request, id):
+    q = Usuario.objects.get(pk = id)
+    contexto = {'data': q}
+
+    return render(request, 'planning_travel/login/usuarios_form_editar.html', contexto)
+
+def usuarios_actualizar(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        nombre = request.POST.get('nombre')
+        correo = request.POST.get('correo')
+        contrasena = request.POST.get('contrasena')
+        rol = Rol.objects.get(pk=request.POST.get('rol'))
+        foto = request.POST.get('foto')
+        try:
+            q = Usuario.objects.get(pk = id)
+            q.nombre = nombre
+            q.correo = correo
+            q.contrasena = contrasena
+            q.rol = rol
+            q.foto = foto
+            q.save()
+            messages.success(request, "Fue actualizado correctamente")
+        except Exception as e:
+            messages.error(request,f'Error: {e}')
+    else:
+        messages.warning(request,'No se enviaron datos')
+
+    return redirect('usuarios_listar')
+
+# Crud de Hoteles
+def hoteles(request):
+    q = Hotel.objects.all()
+    contexto = {'data': q}
+    return render(request, 'planning_travel/hoteles/hoteles.html', contexto)
+
+def hoteles_form(request):
+    q = Categoria.objects.all()
+    contexto = {'data': q}
+    return render(request, 'planning_travel/hoteles/hoteles_form.html', contexto)
+
+def hoteles_crear(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        descripcion = request.POST.get('descripcion')
+        direccion = request.POST.get('direccion')
+        categoria = Categoria.objects.get(pk=request.POST.get('categoria'))
+        cantidad_habitacion = request.POST.get('cantidad_habitacion')
+        try:
+            q = Hotel(
+                nombre=nombre,
+                descripcion=descripcion,
+                direccion=direccion,
+                categoria=categoria,
+                cantidad_habitacion=cantidad_habitacion,
+            )
+            q.save()
+            messages.success(request, "Fue actualizado correctamente")
+        except Exception as e:
+            messages.error(request,f'Error: {e}')
+
+        return redirect('hoteles_listar')
+    else:
+        messages.warning(request,'No se enviaron datos')
+        return redirect('usuarios_listar')
+    
+def hoteles_eliminar(request, id):
+    try:
+        q = Hotel.objects.get(pk = id)
+        q.delete()
+        messages.success(request, 'Hotel eliminado correctamente!!')
+    except Exception as e:
+        messages.error(request,f'Error: {e}')
+
+    return redirect('hoteles_listar')
+
+def hoteles_form_editar(request, id):
+    q = Hotel.objects.get(pk = id)
+    c = Categoria.objects.all()
+    contexto = {'data': q, 'categoria': c}
+
+    return render(request, 'planning_travel/hoteles/hoteles_form_editar.html', contexto)
+
+def hoteles_actualizar(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        nombre = request.POST.get('nombre')
+        descripcion = request.POST.get('descripcion')
+        direccion = request.POST.get('direccion')
+        categoria = Categoria.objects.get(pk=request.POST.get("categoria"))
+        cantidad_habitacion = request.POST.get('cantidad_habitacion')
+        try:
+            q = Hotel.objects.get(pk = id)
+            q.nombre = nombre
+            q.descripcion = descripcion
+            q.direccion = direccion
+            q.categoria = categoria
+            q.cantidad_habitacion = cantidad_habitacion
+            q.save()
+            messages.success(request, "Fue actualizado correctamente")
+        except Exception as e:
+            messages.error(request,f'Error: {e}')
+    else:
+        messages.warning(request,'No se enviaron datos')
+
+# Crud de puntuacion
+def puntuaciones(request):
+    q = Puntuacion.objects.all()
+    contexto = {'data': q}
+    return render(request, 'planning_travel/puntuaciones/puntuaciones.html', contexto)
+
+def puntuaciones_form(request):
+    q = Comentario.objects.all()
+    contexto = {'data': q}
+    return render(request, 'planning_travel/puntuaciones/puntuaciones_form.html', contexto)
+
+def puntuaciones_crear(request):
+    if request.method == 'POST':
+        comentario = Comentario.objects.get(pk=request.POST.get('comentario'))
+        valoracion = request.POST.get('valoracion')
+        try:
+            q = Puntuacion(
+                comentario=comentario,
+                valoracion=valoracion,
+            )
+            q.save()
+            messages.success(request, "Fue actualizado correctamente")
+        except Exception as e:
+            messages.error(request,f'Error: {e}')
+
+        return redirect('puntuaciones_listar')
+    else:
+        messages.warning(request,'No se enviaron datos')
+        return redirect('puntuaciones_listar')
+    
+def puntuaciones_eliminar(request, id):
+    try:
+        q = Puntuacion.objects.get(pk = id)
+        q.delete()
+        messages.success(request, 'Puntuacion eliminada correctamente!!')
+    except Exception as e:
+        messages.error(request,f'Error: {e}')
+
+def puntuaciones_form_editar(request, id):
+    q = Puntuacion.objects.get(pk = id)
+    c = Comentario.objects.all()
+    contexto = {'data': q, 'comentario': c}
+    return render(request, 'planning_travel/puntuaciones/puntuaciones_form_editar.html', contexto)
+
+def puntuaciones_actualizar(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        comentario = Comentario.objects.get(pk=request.POST.get('comentario'))
+        valoracion = request.POST.get('valoracion')
+        try:
+            q = Usuario.objects.get(pk = id)
+            q.comentario = comentario
+            q.valoracion = valoracion
+            q.save()
+            messages.success(request, "Fue actualizado correctamente")
+        except Exception as e:
+            messages.error(request,f'Error: {e}')
+    else:
+        messages.warning(request,'No se enviaron datos')
+
+    return redirect('puntuaciones_listar')
+
